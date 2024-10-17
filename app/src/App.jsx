efbebf69b3ef54686be8e2ee09163dbdc68731ba
Bhavya1820 <bhavyasoni1820@gmail.com>
2024-10-17 23:42:25 +0530

    set Header

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,63 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>App</div>;
+  return <Container>
+    <TopContainer>
+      <div className="logo">
+        <img src="public/Foody Zone.png" alt="logo" />
+      </div>
+
+      <div className="search">
+        <input placeholder="Search Food" />
+      </div>
+    </TopContainer>
+
+    <FilterContainer>
+    <Button>All</Button>
+    <Button>Breakfast</Button>
+    <Button>Lunch</Button>
+    <Button>Dinner</Button>
+    </FilterContainer>
+    </Container>;
+    
 };
 
 export default App;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+const TopContainer = styled.section`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  align-items: center;
+
+  .search{
+    input{
+      background-color: transparent;
+      border: 1px solid red;
+      color: white;
+      border-radius: 5px;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 10px;
+    }
+  }
+`;
+
+const FilterContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+`;
+
+const Button = styled.button`
+  background: #ff4343;
+  border-radius: 5px;
+  padding: 6px 12px;
+  border: none;
+  color: white;
+`;
