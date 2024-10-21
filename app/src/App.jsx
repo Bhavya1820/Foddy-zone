d81cd9a41ef2878c12d7cf67d4e40759122a86ee
Bhavya1820 <bhavyasoni1820@gmail.com>
2024-10-21 13:28:46 +0530

    Complete the code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import SearchResult from "./components/SearchResult/SearchResult";
 
 
-    const BASE_URL = "http://localhost:9000/"
+export const BASE_URL = "http://localhost:9000"
 
 const App = () => {
 
     const [data, setData] = useState(null);
+    const [filteredData, setFilteredData] = useState(null)
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null)
+    const [selectedBtn, setSelectedBtn] = useState("all");
 
     useEffect(() => {
       const fetchFoodData = async() => {
@@ -20,6 +23,7 @@ const App = () => {
           const json = await response.json();
   
           setData(json);
+          setFilteredData(json);
           setLoading(false)
         } catch (error) {
           setError("Unable to fetch data")
@@ -29,46 +33,93 @@ const App = () => {
       fetchFoodData();
     }, [])
 
-    console.log(data);
+    const searchFood = (e) => {
+      const searchValue = e.target.value;
+
+      if(searchValue == ""){
+        setFilteredData(null);
+      }
+
+      const filter = data?.filter((food) => 
+      food.name.toLowerCase().includes(searchValue.toLowerCase()));
+
+      setFilteredData(filter);
+
+    }
+
+    const filterFood = (type) => {
+      if(type == "all"){
+        setFilteredData(data);
+        setSelectedBtn("all")
+        return;
+      }
+
+      const filter = data?.filter((food) => 
+        food.type.toLowerCase().includes(type.toLowerCase()));
+  
+        setFilteredData(filter);
+        setSelectedBtn(type);
+    }
+
+    const filterBtns = [
+      {
+        name: "All",
+        type: "all",
+      },
+      {
+        name: "Breakfast",
+        type: "breakfast",
+      },
+      {
+        name: "Lunch",
+        type: "lunch",
+      },
+      {
+        name: "Dinner",
+        type: "dinner",
+      },
+    ]
 
     if(error) return <div>{error}</div>
     if(loading) return <div>loading...........</div>
 
-  return <Container>
+  return (
+    <>
+    <Container>
     <TopContainer>
       <div className="logo">
         <img src="public/Foody Zone.png" alt="logo" />
       </div>
 
       <div className="search">
-        <input placeholder="Search Food" />
+        <input onChange={searchFood} placeholder="Search Food" />
       </div>
     </TopContainer>
 
     <FilterContainer>
-    <Button>All</Button>
-    <Button>Breakfast</Button>
-    <Button>Lunch</Button>
-    <Button>Dinner</Button>
+      {
+        filterBtns.map((value) => (
+          <Button isSelected={selectedBtn === value.type} key={value.name} onClick={() => filterFood(value.type)}>{value.name}</Button>
+        ))
+      }
     </FilterContainer>
 
-    <FoodCardContainer>
-      <FoodCards>
-
-      </FoodCards>
-    </FoodCardContainer>
     </Container>;
+
+    <SearchResult data={filteredData}></SearchResult>
+    </>
+  )
     
 };
 
 export default App;
 
-const Container = styled.div`
+export const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
 `;
 const TopContainer = styled.section`
-  min-height: 140px;
+  height: 140px;
   display: flex;
   justify-content: space-between;
   padding: 16px;
@@ -83,7 +134,15 @@ const TopContainer = styled.section`
       height: 40px;
       font-size: 16px;
       padding: 0 10px;
+      &::placeholder{
+        color: white;
+      }
     }
+  }
+
+  @media (0 < width < 600px){
+    flex-direction: column;
+    height: 120px;
   }
 `;
 
@@ -94,20 +153,16 @@ const FilterContainer = styled.section`
   padding-bottom: 40px;
 `;
 
-const Button = styled.button`
-  background: #ff4343;
+export const Button = styled.button`
+  background: ${({isSelected}) => (isSelected ? "#f22f2f" : "#ff4343")};
+  outline: 1px solid ${({isSelected}) => (isSelected ? "white" : "#ff4343")};
   border-radius: 5px;
   padding: 6px 12px;
   border: none;
   color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #f22f2f;
+  }
 `;
 
-const FoodCardContainer = styled.section`
-  height: calc(100vh - 210px);
-  background-image: url("/public/bg.png");
-  background-size: cover;
-`;
-
-const FoodCards = styled.div`
-
-`;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -13,6 +13,7 @@ import { createGlobalStyle } from "styled-components";
       background-color: #323334;
       color: white;
       min-height: 100vh;
+      font-family: 'Inter', sans-serif;
     }
   `;
 
